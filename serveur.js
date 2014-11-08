d7a9b83f9b49cca694802f9b69e4d65ba8b0a3b6
BAROUDI Soufiane <bd.soufiane@gmail.com>
2014-11-08 17:18:28 +0100

    mise à jour 2

--- serveur.js
+++ serveur.js
@@ -1,13 +1,37 @@
-	var PORT = 8000;
-	var express = require('express');
-	var app = express();
+var express = require('express')
+var app = express()
 
-	app.get('/', function(req, res){
-	  res.sendFile(__dirname + '/public/index.html');
-	  });
-	
-	app.use(express.static(__dirname + 'public'));
+// Répertoire contenant les données statiques
+app.use(express.static(__dirname + '/public'));
 
-	var server = app.listen(PORT, function(){
-		console.log('le Serveur http écoute %d', server.address().port);
-	});
+// Réponse à la requête permettant d'obtenir les données d'une table
+app.get('/data/:table', function(req, res){
+	// chargement des données
+	var table = tables[req.params.table];
+
+	// envoi des données au format JSON
+	res.json(table);
+});
+
+// Démarrage du serveur
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+  console.log('Serveur écoute http://localhost:%s', port);
+});
+
+// Les données des tables 
+// (typiquement ces données seraient enregistrées en base de données)
+var tables = { 
+    'Annick' : {
+      mac : "32:5b:c4:44:09:d5",
+      state : "OFF",
+      place : "Brest",
+      keg : []
+    },
+    'Annegrete' : {
+      mac : "48:17:e2:cb:21:f4",
+      state : "OFF",
+      place : "Brest",
+      keg : []
+    }
+  };
