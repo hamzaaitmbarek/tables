7847edfdf18ec90a120c1fc4dddc91d8f3940fce
hamza aitmbarek <hamza.aitmbarek@edu.uiz.ac.ma>
2014-11-09 12:31:25 +0100

    configuration de json

--- public/js/app.js
+++ public/js/app.js
@@ -1,71 +1,31 @@
 (function() {
   var app = angular.module('tpAngular', []);
-
+	var tables;
   app.controller('ListController', ['$scope', '$http', function($scope, $http){
+
     // Affichage de la liste des tables
-    $scope.tables = tables;  
+	$http.get("/listeTables").success(function(data){
+		tables = data;
+		$scope.tables = data;
+	}).error(function(errmsg){
+		alert("impossible de charger la liste des tables");
+	});
+
+      
 
     // Fonction permettant l'affichage des détails
     $scope.toggle = function(table){
       // Requete GET pour réupérer les données de la table
-      $http.get('/data/' + table.name).success(function(data){
+      $http.get('/data/' + table.id).success(function(data){
         // enregistrement local des données
-        table.mac = data.mac;
-        table.state = data.state;
-        table.place = data.place;
+       table.mac = data.addresse;
+        table.state = data.etat;
+        table.place = data.lieu.nom;
         // affichage des détails
         table.details = ! table.details;
-      });      
+      }).error(function(errmsg){
+		alert("impossible de charger la table");
+	});      
     }
-
   }]);
-
-
-var table = function(_id,_nom,_addresse,_etat,_img,_details,_lieu) {
-  this.id = _id;
-  this.nom = _nom;
-  this.addresse = _addresse;
-  this.etat = _etat;
-  this.img = _img;
-  this.details = _details;
-  this.lieu=_lieu;
-};
-var lieu = function(_id,_nom,_position) {
-  this.id = _id;
-  this.nom = _nom;
-  this.position = _position;
-};
-var fut = function(_id,_volume,_boisson) {
-  this.id = _id;
-  this.volume = _volume;
-  this.boisson = _boisson;
-};
-var boisson = function(_id,_fabricant,_logo,_type,_img,_description,_lieu) {
-  this.id = _id;
-  this.fabricant = _fabricant;
-  this.logo = _logo;
-  this.type = _type;
-  this.description = _description;
-};
-var prix = function(_lieu,_boisson,_prix) {
-  this.lieu = _lieu;
-  this.boisson = _boisson;
-  this.prix = _prix;
-};
-
-var lieux = [
-      new lieu(0,'brest','a'),
-      new lieu(1,'paris','b')
-
-    ];
-  var tables = [
-      new table(1,'Annick','brest','OFF','images/table.png',false,lieux[0]),
-      new table(2,'Annegrete','paris','OFF','images/table.png',false,lieux[1])
-
-    ];
-
-
-
-
-
 })();
--- serveur.js
+++ serveur.js
@@ -4,6 +4,13 @@ var app = express()
 // Répertoire contenant les données statiques
 app.use(express.static(__dirname + '/public'));
 
+// Réponse à la requête permettant d'obtenir la liste des tables
+app.get('/listeTables', function(req, res){
+	
+	// envoi des données au format JSON
+	res.json(tables);
+});
+
 // Réponse à la requête permettant d'obtenir les données d'une table
 app.get('/data/:table', function(req, res){
 	// chargement des données
@@ -21,17 +28,45 @@ var server = app.listen(3000, function () {
 
 // Les données des tables 
 // (typiquement ces données seraient enregistrées en base de données)
-var tables = { 
-    'Annick' : {
-      mac : "32:5b:c4:44:09:d5",
-      state : "OFF",
-      place : "Brest",
-      keg : []
-    },
-    'Annegrete' : {
-      mac : "48:17:e2:cb:21:f4",
-      state : "OFF",
-      place : "Brest",
-      keg : []
-    }
-  };
+var table = function(_id,_nom,_addresse,_etat,_img,_details,_lieu) {
+  this.id = _id;
+  this.nom = _nom;
+  this.addresse = _addresse;
+  this.etat = _etat;
+  this.img = _img;
+  this.details = _details;
+  this.lieu=_lieu;
+};
+var lieu = function(_id,_nom,_position) {
+  this.id = _id;
+  this.nom = _nom;
+  this.position = _position;
+};
+var fut = function(_id,_volume,_boisson) {
+  this.id = _id;
+  this.volume = _volume;
+  this.boisson = _boisson;
+};
+var boisson = function(_id,_fabricant,_logo,_type,_img,_description,_lieu) {
+  this.id = _id;
+  this.fabricant = _fabricant;
+  this.logo = _logo;
+  this.type = _type;
+  this.description = _description;
+};
+var prix = function(_lieu,_boisson,_prix) {
+  this.lieu = _lieu;
+  this.boisson = _boisson;
+  this.prix = _prix;
+};
+
+var lieux = [
+      new lieu(0,'brest','a'),
+      new lieu(1,'paris','b')
+
+    ];
+  var tables = [
+      new table(1,'Annick','brest','OFF','images/table.png',true,lieux[0]),
+      new table(2,'Annegrete','paris','OFF','images/table.png',false,lieux[1])
+
+    ];
