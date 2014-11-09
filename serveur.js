ff8dfa5c90e5277a50dd45d2c25df299f6277d48
BAROUDI Soufiane <bd.soufiane@gmail.com>
2014-11-09 13:07:49 +0100

    mise à jour 3

--- serveur.js
+++ serveur.js
@@ -42,20 +42,20 @@ var lieu = function(_id,_nom,_position) {
   this.nom = _nom;
   this.position = _position;
 };
-var fut = function(_id,_volume,_boisson_table) {
+var fut = function(_id,_volume,_boisson,_table) {
   this.id = _id;
   this.volume = _volume;
   this.boisson = _boisson;
   this.table = _table;
 };
-var boisson = function(_id,_fabricant,_logo,_type,_img,_description,_lieu) {
+var boisson = function(_id,_fabricant,_logo,_type,_description) {
   this.id = _id;
   this.fabricant = _fabricant;
   this.logo = _logo;
   this.type = _type;
   this.description = _description;
 };
-var prix = function(_lieu,_boisson,_prix) {
+var prix_lieu = function(_lieu,_boisson,_prix) {
   this.lieu = _lieu;
   this.boisson = _boisson;
   this.prix = _prix;
@@ -66,13 +66,25 @@ var lieux = [
       new lieu(1,'paris','b')
 
     ];
-var futs = [
-      new lieu(0,'10l','coca',tables[0]),
-      new lieu(1,'50l','jus',tables[1])
 
-    ];
   var tables = [
-      new table(1,'Annick','54:25:36:j5','OFF','images/table.png',false,lieux[0]),
-      new table(2,'Annegrete','f4:58:4g:fz','OFF','images/table.png',false,lieux[1])
+      new table(0,'Annick','54:25:36:j5','OFF','images/table.png',false,lieux[0]),
+      new table(1,'Annegrete','f4:58:4g:fz','OFF','images/table.png',false,lieux[1])
 
     ];
+  var boissons= [
+      new boisson(0,'DOSI','logo1','boisson chaud','café'),
+      new boisson(1,'DOSI','logo2','boisson froid','coca-cola')
+
+  ];
+  var futs = [
+      new fut(0,'10l',boissons[0],tables[0]),
+      new fut(1,'50l',boissons[1],tables[1])
+
+  ];
+
+  var prix_lieux = [
+      new prix_lieu(lieux[0],boissons[0],'1$'),
+      new prix_lieu(lieux[1],boissons[1],'2$')
+
+  ];
